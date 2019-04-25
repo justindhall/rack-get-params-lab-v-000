517b36b04222097afbec08e5504a5f4a4e57ed63
Justin Hall <hall.justin.d@gmail.com>
2019-04-25 20:54:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,13 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/add/)
-
+      item = req.params["q"]
+      if @@items.include? '#{item}'
+        cart << item
+        resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
