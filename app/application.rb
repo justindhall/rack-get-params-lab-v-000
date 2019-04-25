a6274b3f4e45045f812e0eeccf8277f741f306a3
Justin Hall <hall.justin.d@gmail.com>
2019-04-25 20:50:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif  req.path.math(/cart/)
+    elsif  req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
